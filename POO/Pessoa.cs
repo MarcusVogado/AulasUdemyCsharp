eaf51cf22fea5bce97fdf7e3f2cb608e29167688
marcus vogado <marcus_620@hotmail.com>
2023-01-21 14:21:12 -0300

    update, POO

---
+++ POO/Pessoa.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    public class Pessoa
+    {
+        
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public int AnoDeNascimento{ get; set; }
+        //MÉTODO SIMPLES SEM RETORNO VOID
+        public void Comprimentar()
+        {
+            Console.WriteLine("Olá "+this.Nome+" "+this.SobreNome);
+        }
+        //MÉTODO COM PARÂMETROS      
+        public void Apresentar(string nome,int anoNascimento)
+        {
+            int idade = DateTime.Now.Year-anoNascimento;
+            Console.WriteLine("Olá " + nome + " de idade= " + idade);
+        }
+        //MÉTODO POR VALOR
+        public void AumentarValor(int valor)
+        {
+            valor += 10;
+            Console.WriteLine("Valor Final é: " + valor);
+        }
+        //MÉTODO POR REFERÊNCIA
+        public void AumentarValorRef(ref int valor)
+        {
+            valor += 15;
+            Console.WriteLine("Valor Final é: " + valor);
+        }
+    }
+}
---
+++ POO/Program.cs
@@ -0,0 +1,45 @@
+﻿using POO;
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        MinhaClasse mClasse = new MinhaClasse();
+        MinhaClasse mrClasse = mClasse;
+
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.Nome = "Marcus";
+        pessoa1.SobreNome = "Vogado";
+        pessoa1.AnoDeNascimento = 1993;
+
+        Pessoa pessoa2 = new Pessoa()
+        {
+            Nome = "Junior",
+            SobreNome = "Alberto",
+            AnoDeNascimento = 1998
+        };
+        List<Pessoa> pessoaList = new List<Pessoa>()
+        {
+            new Pessoa(){Nome="Carlos",SobreNome="Vogado",AnoDeNascimento=1993},
+            new Pessoa(){Nome="José",SobreNome="Alberto",AnoDeNascimento=1987} 
+        };
+        pessoaList.Add(pessoa1);
+        pessoaList.Add(pessoa2);
+        foreach(Pessoa pessoa in pessoaList)
+        {
+            //método simples
+            pessoa.Comprimentar();
+            //método com parâmetros
+            pessoa.Apresentar(pessoa.Nome, pessoa.AnoDeNascimento);
+        }
+        int valor1 = 100;
+        int valorRef = 100;
+
+        pessoa1.AumentarValor(valor1);
+        pessoa1.AumentarValorRef(ref valorRef);
+
+
+        Console.ReadKey();
+    }
+}
